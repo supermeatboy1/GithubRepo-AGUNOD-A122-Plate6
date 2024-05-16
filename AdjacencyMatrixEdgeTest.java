daee3e4aff9dade947bbef3856be105599cee420
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2024-05-16 12:17:18 +0800

    Create AdjacencyMatrixEdgeTest.java

---
+++ AdjacencyMatrixEdgeTest.java
@@ -0,0 +1,16 @@
+package ave.maria;
+
+import java.util.Scanner;
+
+public class AdjacencyMatrixEdgeTest {
+
+	public static void run() throws InterruptedException {
+		Scanner scan = new Scanner(
+			CycleTest.class.getClassLoader().getResourceAsStream("test_cases/p2_adjmat_0.txt")
+		);
+		while (scan.hasNextLine()) {
+			System.out.println(scan.nextLine());
+		}
+		scan.close();
+	}
+}
