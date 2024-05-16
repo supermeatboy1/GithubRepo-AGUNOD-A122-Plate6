6060d6a79eb98f9e949cae392a70db56f10b7874
supermeatboy1 <124642134+supermeatboy1@users.noreply.github.com>
2024-05-16 12:17:47 +0800

    Create DSGraphsMain.java

---
+++ DSGraphsMain.java
@@ -0,0 +1,9 @@
+public class DSGraphsMain {
+	public static void main(String[] args) {
+		try {
+			AdjacencyMatrixEdgeTest.run();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+}
